feb453296ef747d2d87313000485f2f3fef9aa5b
Jesper Nylend <jesper.nylend@gmail.com>
2018-10-29 10:42:41 +0100

    Adding Map params and overrides

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,25 +1,37 @@
-def call(def sayDuringCompile, def sayDuringTests, def sayDuringBuild) {
+def call(Map overrides) {
+
+	def pipelineParams = [sayDuringCompile:'Default compile message', sayDuringTests:'Default tests message', sayDuringBuild:'Default build message']
+	if (overrides.containsKey(sayDuringCompile)) {
+		pipelineParams.sayDuringCompile = overrides.sayDuringCompile
+	}
+	if (overrides.containsKey(sayDuringTests)) {
+		pipelineParams.sayDuringTests = overrides.sayDuringTests
+	}
+	if (overrides.containsKey(sayDuringBuild)) {
+		pipelineParams.sayDuringBuild = overrides.sayDuringBuild
+	}
+
 
 	pipeline {
 		agent any
 		stages {
 			stage('Compile') {
 				steps {
-					sh "echo ${sayDuringCompile}"
+					sh "echo ${pipelineParams.sayDuringCompile}"
 					sh 'mvn compile'
 				}
 			}
 			
 			stage('Test') {
 				steps {
-					sh "echo ${sayDuringTests}"
+					sh "echo ${pipelineParams.sayDuringTests}"
 					sh 'mvn test'
 				}
 			}
 
 			stage('Build') {
 				steps {
-					sh "echo ${sayDuringBuild}"
+					sh "echo ${pipelineParams.sayDuringBuild}"
 					sh 'mvn clean package -DskipTests'
 				}
 			}
